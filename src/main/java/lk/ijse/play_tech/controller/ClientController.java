099499b9b11ab834bcffc8767cfccadb437a5f1f
Sanduni253127 <hazinika253127@gmail.com>
2023-07-13 22:17:44 +0530

    update code

--- src/main/java/lk/ijse/play_tech/controller/ClientController.java
+++ src/main/java/lk/ijse/play_tech/controller/ClientController.java
@@ -91,6 +91,12 @@ public class ClientController extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        /*String jaString = new String("\u65e5\u672c\u8a9e\u6587\u5b57\u5217");
+        writeOutput(jaString);
+        String inputString = readInput();
+        String displayString = jaString + " " + inputString;
+        new ShowString(displayString, "Conversion Demo");*/
     }
 
     @Override
@@ -227,7 +233,36 @@ public class ClientController extends Thread {
         btnSend.requestFocus();
     }
 
+    static void writeOutput(String str) {
+        try {
+            FileOutputStream fos = new FileOutputStream("test.txt");
+            Writer out = new OutputStreamWriter(fos, "UTF8");
+            out.write(str);
+            out.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    static String readInput() {
+        StringBuffer buffer = new StringBuffer();
+        try {
+            FileInputStream fis = new FileInputStream("test.txt");
+            InputStreamReader isr = new InputStreamReader(fis, "UTF8");
+            Reader in = new BufferedReader(isr);
+            int ch;
+            while ((ch = in.read()) > -1) {
+                buffer.append((char)ch);
+            }
+            in.close();
+            return buffer.toString();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
     /*public void initialize(int rNum) {
